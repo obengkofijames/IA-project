efa676e3f3a42e7b4e0e363622b2ce954229ab5d
obengkofijames <78661305+obengkofijames@users.noreply.github.com>
2021-10-30 11:31:00 +0000

    Added the search bar and category field to the item screen

--- app/config/colors.js
+++ app/config/colors.js
@@ -2,5 +2,5 @@ import { LinearGradient } from "expo-linear-gradient"
 export default{
     primary:"#fc5c65",
     secondary:"#4ecdc4",
-    killed:"#000000"
+    killed:"#000000",
 }
--- app/screens/Items.js
+++ app/screens/Items.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { View } from "react-native";
-import { Ionicons, FontAwesome, AntDesign } from "@expo/vector-icons";
+import { View,StyleSheet,Text, TouchableOpacity } from "react-native";
+import { Ionicons,EvilIcons,MaterialCommunityIcons } from "@expo/vector-icons";
+import { TextInput } from "react-native-gesture-handler";
+import colors from "../config/colors";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function Items(props) {
+  
+    // category
+  const categories= ["General","Official","Summer","Winter"];
+  const [categoryIndex,setCategoryIndex] = React.useState(0)
+
+  const CategoriesList = () =>{
+    return <View style={styles.categoriesContainer}>
+      {categories.map((items,index) =>(
+        <TouchableOpacity key={index}
+        activeOpacity={0.8}
+         onPress ={ () => setCategoryIndex(index)}>
+          <Text  style={[styles.categoryText, categoryIndex ==index && styles.categoryTextSelected]}>{items}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>;
+  };
+
+
   return (
     <View
       style={{
         backgroundColor: "white",
         flex: 1,
-        paddingTop: 55,
+        paddingTop:55,
         paddingHorizontal: 20,
       }}
     >
@@ -21,11 +42,75 @@ export default function Items(props) {
       >
         <Ionicons name="ios-menu-outline" size={24} color="black" />
         <Ionicons name="cart-sharp" size={24} color="black" />
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <AntDesign name="search1" size={20} color="black" />
-          <Ionicons name="notifications-outline" size={20} color="black" />
-        </View>
+        <Ionicons name="notifications-outline" size={20} color="black" />
       </View>
+
+        
+        {/* search btn */}
+      <View style={{marginTop:30,flexDirection:"row"}}>
+        <View style={styles.searchContainer}>
+        <EvilIcons name="search" size={24} color="black" style={{paddingLeft:10}} />
+        <TextInput placeholder="Search" style={styles.searchText}/>
+        </View>
+
+        {/* sortIcon */}
+        <View style={styles.sortBtn}>
+        <MaterialCommunityIcons name="sort-variant" size={24} color="black" />
+        </View>
+
+      </View>
+
+      {/* categories */}
+      <CategoriesList />
     </View>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  searchContainer:{
+    flex:1,
+    height:50,
+    borderRadius:10,
+    alignItems:"center",
+    backgroundColor:Colors.primary,
+    flexDirection:"row",
+  },
+
+  searchText:{
+    fontSize:18,
+    fontWeight:"bold",
+    color:colors.killed,
+    flex:1,
+  },
+
+  sortBtn:{
+    backgroundColor:"red",
+    marginLeft:10,
+    borderRadius:7,
+    width:50,
+    height:50,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+
+  categoriesContainer:{
+    flexDirection:"row",
+    marginTop:20,
+    marginBottom:20,
+    justifyContent:"space-between",
+  },
+
+  categoryText:{
+    fontSize:16,
+    fontWeight:"bold",
+    color:"grey",
+  },
+
+  categoryTextSelected:{
+    color:Colors.primary,
+    paddingBottom:5,
+    borderBottomWidth:2,
+    borderColor:Colors.primary,
+  },
+})
